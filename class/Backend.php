10eb48089ec1d64c720548d3362ed65519a6b8ed
DQNEOO <DQNEOO@gmail.com>
2013-06-11 17:35:10 +0000

    refactor (var -> public)

--- class/Backend.php
+++ class/Backend.php
@@ -24,49 +24,49 @@ class Ethna_Backend
      */
 
     /** @protected    object  Ethna_Controller    controllerオブジェクト */
-    var $controller;
+    public $controller;
 
     /** @protected    object  Ethna_Controller    controllerオブジェクト($controllerの省略形) */
-    var $ctl;
+    public $ctl;
 
     /** @protected    object  Ethna_ClassFactory  クラスファクトリオブジェクト */
-    var $class_factory;
+    public $class_factory;
 
     /** @protected    object  Ethna_Config        設定オブジェクト */
-    var $config;
+    public $config;
 
     /** @protected    object  Ethna_I18N          i18nオブジェクト */
-    var $i18n;
+    public $i18n;
 
     /** @protected    object  Ethna_ActionError   アクションエラーオブジェクト */
-    var $action_error;
+    public $action_error;
 
     /** @protected    object  Ethna_ActionError   アクションエラーオブジェクト($action_errorの省略形) */
-    var $ae;
+    public $ae;
 
     /** @protected    object  Ethna_ActionForm    アクションフォームオブジェクト */
-    var $action_form;
+    public $action_form;
 
     /** @protected    object  Ethna_ActionForm    アクションフォームオブジェクト($action_formの省略形) */
-    var $af;
+    public $af;
 
     /** @protected    object  Ethna_ActionClass   アクションクラスオブジェクト */
-    var $action_class;
+    public $action_class;
 
     /** @protected    object  Ethna_ActionClass   アクションクラスオブジェクト($action_classの省略形) */
-    var $ac;
+    public $ac;
 
     /** @protected    object  Ethna_Session       セッションオブジェクト */
-    var $session;
+    public $session;
 
     /** @protected    object  Ethna_Plugin        プラグインオブジェクト */
-    var $plugin;
+    public $plugin;
 
     /** @protected    array   Ethna_DBオブジェクトを格納した配列 */
-    var $db_list;
+    public $db_list;
 
     /** @protected    object  Ethna_Logger        ログオブジェクト */
-    var $logger;
+    public $logger;
 
     /**#@-*/
 
